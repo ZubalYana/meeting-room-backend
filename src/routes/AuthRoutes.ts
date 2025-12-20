7d262dde74a50c29f211a821d358857cb5a9fd05
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-20 05:42:28 +0200

    add login endpoint

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -28,4 +28,31 @@ router.post('/register', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required." });
+        }
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password." });
+        }
+
+        const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1d' });
+
+        res.status(200).json({ token: token });
+    }
+    catch (err) {
+        console.log('Error logging user in:', err);
+        res.status(500).json({ message: 'Server error' });
+    }
+})
+
 export default router;
